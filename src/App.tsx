3eed14ad6fcf02a11602f8f9439ee8e83df937a4
Leonid Kuzmychenko <lleonidk@ukr.net>
2024-12-02 09:35:57 +0200

    fix pages

--- src/App.tsx
+++ src/App.tsx
@@ -25,19 +25,26 @@ const App: React.FC = () => {
         {id: 'contacts', title: 'Контакты', content: <ContactsSection/>}
     ];
 
-    switch (window.location.pathname) {
+    let path = window.location.pathname;
+    console.log("path = " + path)
+
+    const content = <>
+        <div style={styles.app}>
+            <Menu item={sections.map(item => ({id: item.id, title: item.title}))}/>
+            {sections.map(({id, content}) => (
+                <Section key={id} id={id} content={content}/>
+            ))}
+            <Footer/>
+        </div>
+    </>
+
+    switch (path) {
         case '/my-first-landing':
-            return <>
-                <div style={styles.app}>
-                    <Menu item={sections.map(item => ({id: item.id, title: item.title}))}/>
-                    {sections.map(({id, content}) => (
-                        <Section key={id} id={id} content={content}/>
-                    ))}
-                    <Footer/>
-                </div>
-            </>;
+            return content;
+        case '/my-first-landing/':
+            return content;
         default:
-            return <NotFoundPage/>; // Если маршрут неизвестен
+            return <NotFoundPage/>;
     }
 };
 
